9218fe6c25255ce1180bbd24bc5bce5090e60eeb
pankaj4005 <105104569+pankaj4005@users.noreply.github.com>
2022-05-11 11:06:12 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+	    <title>Three</title>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    
+	    
+	    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+	    <link rel="stylesheet" href="fonts/icomoon/style.css">
+	
+
+	    <link rel="stylesheet" href="css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/jquery-ui.css">
+	    <link rel="stylesheet" href="css/owl.carousel.min.css">
+	    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+	    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+	
+
+	    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	
+
+	    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	
+
+	    <link rel="stylesheet" href="css/aos.css">
+	
+
+	    <link rel="stylesheet" href="css/style.css">
+	    
+	  </head>
+	  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+	  
+	
+
+	  <div id="overlayer"></div>
+	  <div class="loader">
+	    <div class="spinner-border text-primary" role="status">
+	      <span class="sr-only">Loading</span>
+	    </div>
+	  </div>
+	
+
+	  <div class="site-wrap"  id="home-section">
+	
+
+	    <div class="site-mobile-menu site-navbar-target">
+	      <div class="site-mobile-menu-header">
+	        <div class="site-mobile-menu-close mt-3">
+	          <span class="icon-close2 js-menu-toggle"></span>
+	        </div>
+	      </div>
+	      <div class="site-mobile-menu-body"></div>
+	    </div>
+	   
+	    <header class="site-navbar py-1 js-sticky-header site-navbar-target" role="banner">
+	
+
+	      <div class="container">
+	        <div class="row align-items-center">
+	          
+	          <div class="col-6 col-md-3 col-xl-4  d-block">
+	            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0">Three<span class="text-primary">`</span> </a></h1>
+	          </div>
+	
+
+	          <div class="col-12 col-md-9 col-xl-8 main-menu">
+	            <nav class="site-navigation position-relative text-right" role="navigation">
+	
+
+	              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block ml-0 pl-0">
+	                <li><a href="#" class="nav-link">Home</a></li>
+	                <li><a href="#" class="nav-link">Products</a></li>
+	                <li><a href="#" class="nav-link">About Us</a></li>
+	                <li><a href="#contact-section" class="nav-link">Contact</a></li>
+	              </ul>
+	            </nav>
+	          </div>
+	
+
+	
+
+	          <div class="col-6 col-md-9 d-inline-block d-lg-none ml-md-0" ><a href="#" class="site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a></div>
+	
+
+	        </div>
+	      </div>
+	      
+	    </header>
+		
+	
+
+	    
+		<div class="main" style="overflow: hidden;">
+			<div class="container">
+				<div class="row align-items-center justify-content-center text-center">
+					<div class="col-md-12" style="position: relative;">
+	
+
+						<div class="row mb-0">
+							<div class="col-lg-12 mr-auto">
+								<h2 class="section-title">Laptop for the Future</h1>
+								<p class=" lead">The new 14 inch bezeless display oferring a 4k display with touch screen.</p>
+								<div>
+									<a href="#" class="btn btn-primary mr-2 mb-2">Buy Now</a>
+								</div>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+				<div class="row justify-content-center text-center">
+					<div class="col-7 text-center  mb-1">
+						<img src="images/product.png" alt="Image" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</div>
+	
+
+	    <div class="site-section bg-image2 overlay" id="contact-section">
+	      <div class="container">
+	        <div class="row mb-5">
+	          <div class="col-12 text-center">
+	            <h2 class="section-title mb-3 text-white">Contact Us</h2>
+	          </div>
+	        </div>
+	        <div class="row justify-content-center">
+	          <div class="col-lg-7 mb-1">
+	
+
+	            
+	
+
+	            <form action="#" class="p-5 bg-white">
+	
+
+	              <div class="row form-group">
+	                <div class="col-md-6 mb-3 mb-md-0">
+	                  <label class="text-black" for="fname">First Name</label>
+	                  <input type="text" id="fname" class="form-control rounded-0">
+	                </div>
+	                <div class="col-md-6">
+	                  <label class="text-black" for="lname">Last Name</label>
+	                  <input type="text" id="lname" class="form-control rounded-0">
+	                </div>
+	              </div>
+	
+
+	              <div class="row form-group">
+	                
+	                <div class="col-md-12">
+	                  <label class="text-black" for="email">Email</label> 
+	                  <input type="email" id="email" class="form-control rounded-0">
+	                </div>
+	              </div>
+	
+
+	              <div class="row form-group">
+	                <div class="col-md-12">
+	                  <label class="text-black" for="message">Message</label> 
+	                  <textarea name="message" id="message" cols="30" rows="4" class="form-control rounded-0" placeholder="Type your message here..."></textarea>
+	                </div>
+	              </div>
+	
+
+	              <div class="row form-group">
+	                <div class="col-md-12">
+	                  <input type="submit" value="Send Message" class="btn btn-primary mr-2 mb-2">
+	                </div>
+	              </div>
+	
+
+	  
+	            </form>
+	          </div>
+	        
+	        </div>
+	        
+	      </div>
+	    </div>
+	
+
+	  <script src="js/jquery-3.3.1.min.js"></script>
+	  <script src="js/jquery-ui.js"></script>
+	  <script src="js/popper.min.js"></script>
+	  <script src="js/bootstrap.min.js"></script>
+	  <script src="js/owl.carousel.min.js"></script>
+	  <script src="js/jquery.countdown.min.js"></script>
+	  <script src="js/bootstrap-datepicker.min.js"></script>
+	  <script src="js/jquery.easing.1.3.js"></script>
+	  <script src="js/aos.js"></script>
+	  <script src="js/jquery.fancybox.min.js"></script>
+	  <script src="js/jquery.sticky.js"></script>
+	
+
+	  
+	  <script src="js/main.js"></script>
+	  
+	  </body>
+	</html>
